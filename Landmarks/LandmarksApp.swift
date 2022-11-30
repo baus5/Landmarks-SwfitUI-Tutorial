dcdbd4dd2814fd788fda68e18e351d4560b873b1
baus5 <65490597+baus5@users.noreply.github.com>
2022-12-01 01:37:35 +0300

    Added Chapter 1.1 and 1.2
    ch1. Creating and Combining Views
    ch2. Building Lists and Navigation

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by baus on 27.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by baus on 30.11.2022.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (3rd generation)", "iPhone 11", "iPhone 14 Pro"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
